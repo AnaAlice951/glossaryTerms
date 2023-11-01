9843bfc2ffe7f19cfdfcf39be7f4599ad8dd59ca
Ana Alice <aasoares55@gmail.com>
2023-11-01 10:57:31 -0300

    Last changes

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,9 +1,15 @@
 import Header from '../components/header/Hearder';
 import { useData } from '../DataContext';
 import WordBox from '../components/word-box/WordBox';
+import { useState } from 'react';
 
 export function Menu() {
   const data = useData();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredData = data.filter((boxData) => {
+    return boxData.nome.toLowerCase().includes(searchTerm.toLowerCase());
+  });
 
   return (
     <>
@@ -14,12 +20,13 @@ export function Menu() {
           <p>Descubra termos utilizados no contexto de direitos digitais</p>
           <input
             type="text"
-            className="bg-gray-50 border w-4/5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#9742FF] focus:border-[#9742FF] block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#9742FF] dark:focus:border-[#9742FF]"
+            className="bg-gray-50 border w-4/5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#9742FF] focus:border-[#9742FF] block p-2.5  dark:text-white dark:focus:ring-[#9742FF] dark:focus:border-[#9742FF]"
             placeholder="Encontre palavras interresantes..."
-            required
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-4 w-[80%] sm:mt-[3%] mt-[6%] h-full">
-            {data.map((boxData) => (
+            {filteredData.map((boxData) => (
               <WordBox data={boxData} />
             ))}
           </div>
